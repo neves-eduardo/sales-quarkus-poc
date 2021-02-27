733c95286620d68a0390ed773b6889df076eb960
Eduardo Neves <eduardoneves858@gmail.com>
2021-02-27 13:31:12 -0300

    initial commit

---
+++ src/test/java/org/neves/eduardo/sales/quarkus/NativepocIT.java
@@ -0,0 +1,9 @@
+package org.neves.eduardo.sales.quarkus;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativepocIT extends pocTest {
+
+    // Execute the same tests but in native mode.
+}
