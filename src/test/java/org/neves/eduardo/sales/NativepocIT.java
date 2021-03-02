619b48d0369c4eed2f261b539976ece53629f7f6
Eduardo Neves <eduardoneves858@gmail.com>
2021-03-02 02:19:42 -0300

    feat: testing salesman number of sales ordering

--- src/test/java/org/neves/eduardo/sales/NativepocIT.java
+++
@@ -1,9 +0,0 @@
-package org.neves.eduardo.sales;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativepocIT extends pocTest {
-
-    // Execute the same tests but in native mode.
-}
---
+++ src/test/java/org/neves/eduardo/sales/business/sale/SaleServiceTest.java
@@ -0,0 +1,7 @@
+package org.neves.eduardo.sales.business.sale;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SaleServiceTest {
+
+}
